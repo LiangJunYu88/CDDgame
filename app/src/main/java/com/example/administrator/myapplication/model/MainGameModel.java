0c57a8647641f8be49fec5bd606bb43152ffadfd
ZxMapleLiu <610307812@qq.com>
2019-06-05 15:46:00 +0800

    创建了玩家对象，实现了发牌

--- app/src/main/java/com/example/administrator/myapplication/model/MainGameModel.java
+++ app/src/main/java/com/example/administrator/myapplication/model/MainGameModel.java
@@ -1,20 +1,36 @@
 package com.example.administrator.myapplication.model;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 enum color{
     club,diamond,heart,spade
 }
+enum BotType
+{
+    PASSIVE, AGGRESSIVE
+}
 public class MainGameModel {
+    //属性
     static private MainGameModel mainGameModelInstance;
+    public ArrayList<card> cardDeck;//用于存储卡牌的牌堆
+    public playerObject playerInstance;//人类玩家的玩家对象
+    public botObject [] botInstance;
+
+
+    //方法
     private MainGameModel()
     {
         //生成牌堆
         createNewCardDeck();
+        //创建玩家与电脑玩家的对象
+        playerInstance = new playerObject();
+        for(int i=0;i<3;i++)
+            botInstance[i] = new botObject();
+        //发牌
+        distributeCards();
     }
 
-
-    public ArrayList<card> cardDeck;//用于存储卡牌的牌堆
     private void createNewCardDeck()
     {
         cardDeck.clear();
@@ -33,7 +49,23 @@ public class MainGameModel {
     }
     private void distributeCards()
     {
-        //TODO:分发手牌到三位玩家手上
+        Random rand = new Random();
+        //给玩家发牌
+        for(int i=0;i<13;i++)
+        {
+            int currentCardsLeft = cardDeck.size();
+            int index = rand.nextInt(currentCardsLeft);
+            playerInstance.cardsInHand.add(cardDeck.get(index));
+        }
+        //给BOT发牌
+        for(int i=0;i<3;i++)
+        {
+            for (int j = 0; j < 13; j++) {
+                int currentCardsLeft = cardDeck.size();
+                int index = rand.nextInt(currentCardsLeft);
+                botInstance[j].cardsInHand.add(cardDeck.get(index));
+            }
+        }
     }
     public MainGameModel getMainGameModel()
     {
@@ -70,5 +102,29 @@ class card
 
 class playerObject
 {
-    //TODO:创建玩家的类型
+    public playerObject()
+    {
+        //无参构造方法给bot继承
+    }
+    public playerObject(String name)
+    {
+        //为填入玩家姓名预留接口
+        playerName = name;
+    }
+
+    public String playerName;
+    public ArrayList<card> cardsInHand;
+
+
+}
+class botObject extends playerObject
+{
+    botObject()
+    {
+        //懒得实现AI了，全被动（只会pass）完事了
+        super();
+        botType = BotType.PASSIVE;
+    }
+
+    BotType botType;
 }
